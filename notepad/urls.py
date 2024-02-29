8d716d991479707f94ee205acffa851150feb60b
Nishant <nishant2024rde@gmail.com>
2024-02-28 21:41:08 -0800

    Initial Commit

--- notepad/urls.py
+++ notepad/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings 
+from django.conf.urls import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("shadow.urls")),
     path('notepad/',include("note.urls"))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 
