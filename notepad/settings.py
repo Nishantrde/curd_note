826bd09a428973178ddc7aca4ed5f6767828e899
Nishant <nishant2024rde@gmail.com>
2024-02-28 22:44:29 -0800

    Initial Commit

--- notepad/settings.py
+++ notepad/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-17vx8#ll266&pq8kgsz9dhlvilq%re!$7i2bil2!d%gsk43&01
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
