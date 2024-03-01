adf5d49932e433c88065a8c473cb69ab78b4acba
Nishant <nishant2024rde@gmail.com>
2024-02-29 21:57:11 -0800

    Initial Commit

--- note/urls.py
+++ note/urls.py
@@ -5,8 +5,10 @@ urlpatterns = [
     path('', views.sign_in, name="sign_in"),
     path('login', views.index, name="index"),
     path('note', views.notepad, name="notes"),
-    path('save', views.save, name="save"),
     path('diary', views.diary, name="diary"),
     path('user_note', views.note_, name="note"),
+    path('save', views.save, name="save"),
+    path('user_note', views.note_, name="note"),
+    path('delete', views.delete, name="delete"),
 ]
 
