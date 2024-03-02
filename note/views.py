696b6da76988720759012b7d6c4ab83056139774
Nishant <nishant2024rde@gmail.com>
2024-03-02 06:18:17 -0800

    Initial Commit

--- note/views.py
+++ note/views.py
@@ -37,7 +37,7 @@ def save(request):
 
     user_notes = user_notes.replace('\n', '<br>')
     if Notes.objects.filter(user_id = id):
-        obj1 = Notes.objects.get(user_notes_title = user_title)
+        obj1 = Notes.objects.get(user_id = id)
         obj1.user_notes_title = user_title
         obj1.user_notes = user_notes
     else:
