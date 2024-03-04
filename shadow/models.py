b3d54ab4d1119f176f939732ce31644cfa75dba4
Nishant <nishant2024rde@gmail.com>
2024-03-03 22:44:47 -0800

    Initial Commit

--- shadow/models.py
+++ shadow/models.py
@@ -3,9 +3,15 @@ from django.db import models
 class First(models.Model):
     name = models.CharField(max_length = 100)
     age = models.IntegerField()
-    # address = models.TextField(blank = True, null = True)
-    # email = models.EmailField()
-    # img = models.ImageField()
-    # file =  models.FileField()
+    
     def __str__(self):
-        return self.name    
+        return self.name  
+      
+class Second(First):
+    i_d = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
+
+
+
