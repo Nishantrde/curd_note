acb05f88f60283147cd7c113ea735cc52d2ba77b
Nishant <nishant2024rde@gmail.com>
2024-04-10 08:11:37 -0700

    Initial Commit

--- notepad/settings.py
+++ notepad/settings.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 import os
-import dotenv
+from dotenv import load_dotenv
 
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
